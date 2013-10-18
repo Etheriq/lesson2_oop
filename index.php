9b3bfdfbcffc2d462bd4bd0affc8c660186cf8be
Yuriy Tarnavskiy <xeon@home.local>
2013-10-18 13:22:09 +0300

    class monitor class pc abstr class inventory interface inventoryInterface

--- index.php
+++ index.php
@@ -11,7 +11,6 @@ require_once "classes/class.monitor.php";
 require_once "classes/class.pc.php";
 require_once "classes/class.inventory.php";
 
-
 $pc1 = new pc("44003010012356","Pentium 4 3 Ghz", "320 Gb", "2 Gb");
 $pc2 = new pc("44003010056955", "Core2Duo E2140 2.13 Ghz", "160 Gb", "2 Gb");
 
