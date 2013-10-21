ef731ed2fb397bbdd9037c5eaf009a55000a465d
Yuriy Tarnavskiy <xeon@home.local>
2013-10-21 14:44:29 +0300

    приведение текста к стандартам PSR. fix

--- index.php
+++ index.php
@@ -16,14 +16,14 @@ require_once "classes/class.printer.php";
 
 include "autoload.php";
 
-$pc1 = new pc("44003010012356", "Pentium 4 3 Ghz", "320 Gb", "2 Gb");
-$pc2 = new pc("44003010056955", "Core2Duo E2140 2.13 Ghz", "160 Gb", "2 Gb");
+$pc1 = new Pc("44003010012356", "Pentium 4 3 Ghz", "320 Gb", "2 Gb");
+$pc2 = new Pc("44003010056955", "Core2Duo E2140 2.13 Ghz", "160 Gb", "2 Gb");
 
 $pc1->showFullInfo();
 $pc2->sendToService();
 
-$printer1 = new printer("44003010025541", "HP 1320", "Laser printer", "Q5949A");
+$printer1 = new Printer("44003010025541", "HP 1320", "Laser printer", "Q5949A");
 $printer1->showFullInfo();
 
-$mon1 = new monitor("44003010032655","Samsung 710n", "TFT", "D-SUB");
+$mon1 = new Monitor("44003010032655", "Samsung 710n", "TFT", "D-SUB");
 $mon1->showFullInfo();
