21fdab554ea6d0e9253eb9a864859f62e67f7f6f
Yuriy Tarnavskiy <etheriq2@gmail.com>
2013-10-22 23:40:30 +0300

    fix upper case filename all classes

---
+++ classes/class.Monitor.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * File: class.monitor.php
+ * User: Yuriy Tarnavskiy
+ * Date: 18.10.13
+ * Time: 11:03
+ */
+
+//require_once "class.AbstractInventory.php";
+//require_once "interface.inventoryInterface.php";
+
+class Monitor extends AbstractInventory implements InventoryInterface
+{
+    private $model;
+    private $type;
+    private $interf;
+
+    public function getInventNumber()
+    {
+        return $this->inv_number;
+    }
+
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    public function getInterf()
+    {
+        return $this->interf;
+    }
+
+    public function setInventoryNumber($newInventNumber)
+    {
+        $this->inv_number = $newInventNumber;
+    }
+
+    public function setModel($newModel)
+    {
+        $this->model = $newModel;
+    }
+
+    public function setType($newType)
+    {
+        $this->type = $newType;
+    }
+
+    public function setInterf($newInterf)
+    {
+        $this->interf = $newInterf;
+    }
+
+    public function sendToService()
+    {
+        $result = "Monitor with inventory number ".$this->getInventNumber()." and model ".$this->getModel()." sent to service organization.\n";
+        return $result;
+    }
+
+    public function showFullInfo()
+    {
+        $result = "Full information about ".$this->getModel()." monitor:\n
+            Inventory number: ".$this->getInventNumber().";\n
+            Type of monitor: ".$this->getType().";\n
+            Interface of monitor: ".$this->getInterf().";\n";
+        return $result;
+    }
+}
---
+++ classes/interface.InventoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * File: interface.inventoryInterface.php
+ * User: Yuriy Tarnavskiy
+ * Date: 18.10.13
+ * Time: 11:26
+ */
+
+interface InventoryInterface
+{
+    public function sendToService();
+    public function showFullInfo();
+}
